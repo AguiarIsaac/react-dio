e9160bb7c4bce0a6d4f00e1b14f8d1ce8dab3243
AguiarIsaac <isaacaguiar_@hotmail.com>
2021-11-01 16:34:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rqgqru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rqgqru)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Item from '../components/item/index.js';
+
+const App = () => {
+  return (
+    <>
+      <h1>Minha primeira aplicação com ReacJS</h1>
+      <ul>
+        <Item> Item 1 </Item>
+        <Item> Item 2 </Item>
+        <Item> Item 3 </Item>
+      </ul>
+    </>
+  );
+};
+
+export default App;
