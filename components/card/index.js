47302dc05a8a05696a955ed6cfdf96e8ca924f51
AguiarIsaac <isaacaguiar_@hotmail.com>
2021-11-04 14:39:38 -0300

    1 file modified

--- components/card/index.js
+++ components/card/index.js
@@ -4,34 +4,23 @@ import { useState } from 'react';
 const Card = () => {
   const [valor, setValor] = useState(0);
 
+  function Adicionar() {
+    setValor(valor + 1);
+  }
+
+  function Remover() {
+    setValor(valor - 1);
+  }
   return (
     <div className="card">
       <div className="card-body">
-        <button type="button" className="btn btn-success">
+        <button type="button" className="btn btn-success" onClick={Adicionar}>
           Adicionar
         </button>
-        <button type="button" class="btn btn-danger">
+        <button type="button" class="btn btn-danger" onClick={Remover}>
           Remover
         </button>
         <p>{valor}</p>
-        <h5 className="card-title">Meu primeiro Card</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-      </ul>
-      <div className="card-body">
-        <a href="#" className="card-link">
-          Card link
-        </a>
-        <a href="#" className="card-link">
-          Another link
-        </a>
       </div>
     </div>
   );
